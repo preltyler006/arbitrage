140574dfde1964548d1f222dbd9887705d5ae439
Tyler Prellwitz <tylerprellwitz@Tylers-MacBook-Air.local>
2024-09-03 15:10:02 -0700

    added calculateMoneyLineProfitV2

--- arbitrage.py
+++ arbitrage.py
@@ -1,6 +1,9 @@
 def getMoneyLines():
+    '''
+    Originally used for calculateMoneyLineProfitV1, not needed anymore as V2 works much better
+    '''
     # moneylineOne = input("What is the first team and moneyline? Split with a space: ")
-    moneylineOne = "gsw -140"
+    moneylineOne = "gsw +140"
     # moneylineTwo = input("What is the second team and moneyline? Split with a space: ")
     moneylineTwo = "lal -120"
 
@@ -18,7 +21,41 @@ def getMoneyLines():
     return moneylineOneTeam, moneylineOneOdds, moneylineTwoTeam, moneylineTwoOdds
 
 
-def calculateMoneyLineProfit(m1t, m1o, m2t, m2o):
+
+
+
+
+def calculateMoneyLineProfitV2(mt, mo, bet):
+    '''
+    Function returns profit based on three parameters
+    mt ex: buf
+    mo ex: +120
+    bet ex: 100
+    '''
+    team = str(mt)
+    moneyLine = str(mo)
+    bet = int(bet)
+    profit = 0
+
+    if (mo[:1] == "+"): #If moneyline is underdog, +
+        profit = float(float(bet)/100.0) * (float(mo[1:])) - float(bet)
+    else: #If moneyline is favorite, -
+        profit = (float(100/int(mo[1:])) * float(bet))
+
+    print("Betting $" + str(bet) + " on " + team + "'s moneyline of " + str(mo) + " equals $" + str(round(profit, 2)) + " profit.") #Print profit
+
+
+
+
+
+
+
+
+def calculateMoneyLineProfitV1(m1t, m1o, m2t, m2o): 
+    '''
+    Function returns profit on 4 parameters
+    Too many if/else statements, cluttered code, discarded
+    '''
     teamAndBet = input("What is your bet on which team? Seperate with space, (ex: 100 buf) ")
     team = teamAndBet[teamAndBet.index(" ") + 1: ]
     bet = teamAndBet[:teamAndBet.index(" ")]
@@ -47,6 +84,12 @@ def calculateMoneyLineProfit(m1t, m1o, m2t, m2o):
     print("Betting $" + str(bet) + " on " + team + "'s moneyline of " + str(m1o) + " equals $" + str(round(profit, 2)) + " profit.") #Print profit
 
 
-calculateMoneyLineProfit(*getMoneyLines())
+    def arbitrageCalculator():
+        return 0
+
+
+# calculateMoneyLineProfitV1(*getMoneyLines()) # Math works for this one, used as testing for V2
+calculateMoneyLineProfitV2("gsw", "-140", 100) # Input team name, line, and bet manually
+
 
 

e0e7982f60a8df5b0a38ce224b5edad183c4e770
Tyler Prellwitz <tylerprellwitz@Tylers-MacBook-Air.local>
2024-09-07 11:51:06 -0700

    some testing lines and output

--- arbitrageCalculator.py
+++ arbitrageCalculator.py
@@ -4,18 +4,20 @@ from isThereArbitrage import *
 
 def arbitrageBetCalculator(bet, o1, o2):
     '''
-    1st parameter, input total amount wanted to wager
+    1st parameter, input initial amount wanted to wager on 1st bet
     o1, o2 input as string ex: "+130"
     '''
 
     if(isThereArbitrage(o1, o2)[0]): #valid Arbitrage
+        
         print("Arbitrage is possible with a " + str(round((100-(isThereArbitrage(o1, o2)[1])), 2)) + "% profit margin\n")
-        #put $100 on underdog, and reverse engineer amount needed to get same payout on favorite line
-        if (o1[:1] == "+" and o2[:1] == "+"): #if 1st and 2nd line are underdog
-            profitLine1 = float(calculateMoneyLineProfitV2("team", o1, bet))
-            betForLine2 = round(reversePayoutCalculator(profitLine1, o2, bet), 2) #calculates money needed to equal payout for favorite line
-            profitLine2 = float(calculateMoneyLineProfitV2("team", o2, betForLine2))
 
+        if (o1[:1] == "+" and o2[:1] == "+"): #if 1st and 2nd line are underdog
+            profitLine1 = float(calculateMoneyLineProfitV2("team", o1, bet)) #Calculates profit bsaed on original bet for first line
+            betForLine2 = round(reversePayoutCalculator(profitLine1, o2, bet), 2) #Calculates bet needed on second line to equal payout for first line
+            profitLine2 = float(calculateMoneyLineProfitV2("team", o2, betForLine2)) #Calculates profit for second line
+
+            #prints for user to know what to bet where
             print("Betting $" + str(bet) + " on " + o1  + " results in " + str(profitLine1 + bet) + " payout.")
             print("Betting $" + str(betForLine2) + " on " + o2 + " results in " + str(profitLine2 + betForLine2) + " payout.")
             print("\nYour profit will be $" + str(round(((profitLine1 + bet) - (bet + betForLine2)), 2)) + "\n")
@@ -26,6 +28,7 @@ def arbitrageBetCalculator(bet, o1, o2):
             betForLine1 = round(reversePayoutCalculator(profitLine2, o1, bet), 2)
             profitLine1 = round(float(calculateMoneyLineProfitV2("team", o1, betForLine1)), 2)
             
+
             print("bet $" + str(bet) + " on the " + o2 + " line to get " + str(profitLine2 + bet) + " payout for underdog")
             print("bet $" + str(betForLine1) + " on the " + o1 + " line to get " + str(betForLine1 + profitLine1) + " payout for favorite")
             print("\nYour profit will be $" + str(round(((profitLine2 + bet) - (bet + betForLine1)), 2)) + "\n")
@@ -35,12 +38,13 @@ def arbitrageBetCalculator(bet, o1, o2):
             betForLine2 = round(reversePayoutCalculator(profitLine1, o2, bet), 2)
             profitLine2 = round(float(calculateMoneyLineProfitV2("team", o2, betForLine2)), 2)
             
+
             print("\nbet $" + str(bet) + " on the " + o1 + " line to get " + str(profitLine1 + bet) + " payout for underdog")
             print("bet $" + str(betForLine2) + " on the " + o2 + " line to get " + str(betForLine2 + profitLine2) + " payout for favorite")
             print("\nYour profit will be $" + str(round(((profitLine1 + bet) - (bet + betForLine2)), 2)) + "\n")
 
         
-    else:
+    else: #No valid Arbitrage
         print(isThereArbitrage(o1, o2))
         return "Arbitrage Not Possible"
 
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 from arbitrageCalculator import *
 
-os.system("clear")
-arbitrageBetCalculator(1000, "+105", "-100")
+try:
+    os.system("clear")
+except:
+    os.system("cls")
+
+#Run arbitrageBetCalculator function:
+    #1st argument is bet size for 1st line, integer
+    #2nd argument is 1st line, string with + or - in front
+    #3rd argument is 2nd line, string with + or - in front
+arbitrageBetCalculator(100, "-285", "+320")
 # arbitrageBetCalculator(100, "-130", "+110")
 # arbitrageBetCalculator(100, "+100", "-120")

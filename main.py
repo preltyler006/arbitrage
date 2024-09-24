3dcd04a34a09bcda47df1770ebe783020ccc8e85
Tyler Prellwitz <tylerprellwitz@Tylers-MacBook-Air.local>
2024-09-23 17:25:36 -0700

    added code to api file to get information from odds website, added .gitignore and .env to protect API key

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ except:
     #1st argument is bet size for 1st line, integer
     #2nd argument is 1st line, string with + or - in front
     #3rd argument is 2nd line, string with + or - in front
-arbitrageBetCalculator(100, "+240", "-305")
-# arbitrageBetCalculator(100, "-130", "+110")
+# arbitrageBetCalculator(100, "+240", "-305")
+arbitrageBetCalculator(100, "-165", "+240")
 # arbitrageBetCalculator(100, "+100", "-120")

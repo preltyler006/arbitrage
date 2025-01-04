db8177334184baa04516bd75e812adad9c0e78c7
Tyler Prellwitz <tylerprellwitz@Tylers-MacBook-Air.local>
2025-01-03 17:22:58 -0800

    added timer for efficeny tests

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from arbitrageCalculator import *
 from jsonReaderV2 import line_and_team
 from apiGet import *
+import time
+
+start_time = time.perf_counter()
 
 
 # Clear screen
@@ -17,9 +20,15 @@ try:
     writeToDataFile('./apiCallData.txt')
 except:
     print("API error")
+print("\n")
 
 
 # line_and_team is the main function that takes in the data from apiCallData.txt 
 # prints a message for the user based on the bet they want, which is the one argument
 # needed to be inputted as an integer
 print(line_and_team(100))
+end_time = time.perf_counter()
+
+elapsed_time = end_time - start_time
+
+print("\n\n\nElapsed Time: " + str(elapsed_time) + " seconds\n\n\n")
